a049ec778b984451b1c6f4fef05339cbad483ec1
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2022-09-18 00:49:48 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Deploy the template using the SAM CLI
-echo "Sam deploy started..."
+echo "Sam build  started..."
+sam build
+echo "Sam build  finished..."
+echo "Sam deploy  started..."
 sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
 if [ $? -ne 0 ]; then
   echo "SAM deploy error"
