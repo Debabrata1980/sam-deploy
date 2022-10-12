4460a65925a24b5be85877cf0ca0cd05d5eb0b83
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2022-10-12 14:55:38 +0530

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 # Deploy the template using the SAM CLI
+
+args[0]="--template $1"
+
+if [ ! -z $2 ]
+then
+args[1]="--region $2"
+fi
+
+if [ ! -z $3 ]
+then
+args[2]="--config-file $3"
+fi
+
 echo "Sam build  started..."
-sam build
+sam build ${args[@]}
 echo "Sam build  finished..."
 echo "Sam deploy  started..."
-sam deploy --no-confirm-changeset --no-fail-on-empty-changeset
+sam deploy --no-confirm-changeset --no-fail-on-empty-changeset ${args[@]}
 if [ $? -ne 0 ]; then
   echo "SAM deploy error"
   exit 1
