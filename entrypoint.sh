0f0202006d43d36ed8c4bcd02f52b23db99db5b9
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2022-10-12 17:03:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Deploy the template using the SAM CLI
 
-args[0]="--template $1"
+args[0]="--template-file $1"
 
 if [ ! -z $2 ]
 then
@@ -15,8 +15,8 @@ args[2]="--config-file $3"
 fi
 
 #echo "Sam build  started..."
-#sam build ${args[@]}
-#echo "Sam build  finished..."
+sam build ${args[@]}
+echo "Sam build  finished..."
 echo "Sam deploy  started..."
 sam deploy --no-confirm-changeset --no-fail-on-empty-changeset ${args[@]}
 if [ $? -ne 0 ]; then
