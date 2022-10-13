da8e96c9514d1015b48d3987ee45b4a6a6f84e1d
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2022-10-13 15:26:31 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,35 +2,18 @@
 
 # Deploy the template using the SAM CLI
 
-echo "$1"
-echo "$2"
-echo "$3"
-echo "$*"
-
-#args[0]="--template-file $1"
-
-#if [ ! -z $2 ]
-#then
-#args[1]="--region $2"
-#fi
-
-#if [ ! -z $3 ]
-#then
-#args[2]="--config-file $3"
-#fi
-
 template_file="$1"
 region="$2"
 config_file="$3"
 
-#echo "Sam build  started..."
+echo "Sam build  started..."
 
 sam validate -t ${template_file} --region ${region}
 
-sh -c "sam build -t ${template_file} --region ${region}"
+sam build -t ${template_file} --region ${region}
 echo "Sam build  finished..."
 echo "Sam deploy  started..."
-sh -c "sam deploy --config-file ${config_file} -t ${template_file} --no-confirm-changeset --no-fail-on-empty-changeset  --region ${region}"
+sam deploy --config-file ${config_file} -t ${template_file} --no-confirm-changeset --no-fail-on-empty-changeset  --region ${region}
 if [ $? -ne 0 ]; then
   echo "SAM deploy error"
   exit 1
