e63ac450462ddbf838dddbc5ae755d7f90f0e4ed
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2022-10-13 12:11:11 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,25 +9,25 @@ echo "$*"
 
 #args[0]="--template-file $1"
 
-if [ ! -z $2 ]
-then
-args[1]="--region $2"
-fi
+#if [ ! -z $2 ]
+#then
+#args[1]="--region $2"
+#fi
 
-if [ ! -z $3 ]
-then
-args[2]="--config-file $3"
-fi
+#if [ ! -z $3 ]
+#then
+#args[2]="--config-file $3"
+#fi
 
 template_file="$1"
 region="$2"
 config_file="$3"
 
 #echo "Sam build  started..."
-sam build -t ${template_file} --region ${region} --config-file ${config_file}
+sam build  --config-file ${config_file} -t ${template_file} --region ${region}
 echo "Sam build  finished..."
 echo "Sam deploy  started..."
-sam deploy --no-confirm-changeset --no-fail-on-empty-changeset -t ${template_file} --region ${region} --config-file ${config_file}
+sam deploy --config-file ${config_file} -t ${template_file} --no-confirm-changeset --no-fail-on-empty-changeset  --region ${region} 
 if [ $? -ne 0 ]; then
   echo "SAM deploy error"
   exit 1
